0d85354c128e19e4454475c9c754a3c68954bf5a
Saahil <saahilattud@gmail.com>
2023-01-19 15:44:15 -0500

    e

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 let express = require("express"), app = express()
-app.use(express.static(__dirname)), app.listen(process.env.PORT || 2023)
+app.use(express.static(__dirname)), app.listen(process.env.PORT || process.env.SERVER_PORT || 2023)
